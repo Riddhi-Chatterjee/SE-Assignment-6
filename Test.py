b55ada0f4d962a314567b62edeb29c14f20d9318
Riddhi-Chatterjee <riddhich@rediffmail.com>
2022-11-25 01:28:23 +0530

    Modified files

--- Test.py
+++ Test.py
@@ -4,40 +4,34 @@ import unittest
 from Division import divide
 
 class Tests(unittest.TestCase): 
-    def fail_test_case_1(self):
-        a = 10
-        b = 0
-        
-        result = divide(a, b)
-       
-    def pass_test_case_1(self):
+    def test_case_pass_1(self):
         a = 10
         b = 5
         
         result = divide(a, b)
         self.assertEqual(result, 2.0)
         
-    def pass_test_case_2(self):
+    def test_case_pass_2(self):
         a = 1
         b = 4
         
         result = divide(a, b)
         self.assertEqual(result, 0.25)
         
-    def pass_test_case_3(self):
+    def test_case_pass_3(self):
         a = -4
         b = 2
         
         result = divide(a, b)
         self.assertEqual(result, -2.0)
         
-    def fail_test_case_1(self):
+    def test_case_fail_1(self):
         a = 10
         b = 0
         
         result = divide(a, b)
         
-    def fail_test_case_2(self):
+    def test_case_fail_2(self):
         a = 100
         b = 2
         
