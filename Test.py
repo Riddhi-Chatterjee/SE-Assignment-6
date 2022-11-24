e2cee2bf226e6b55f04e655b05ef2cbd77bf2e43
Riddhi-Chatterjee <riddhich@rediffmail.com>
2022-11-25 01:34:51 +0530

    Modified files

--- Test.py
+++ Test.py
@@ -10,6 +10,7 @@ class Tests(unittest.TestCase):
         
         result = divide(a, b)
         self.assertEqual(result, 2.0)
+        print("test_case_pass_1 passed!")
         
     def test_case_pass_2(self):
         a = 1
@@ -17,6 +18,7 @@ class Tests(unittest.TestCase):
         
         result = divide(a, b)
         self.assertEqual(result, 0.25)
+        print("test_case_pass_2 passed!")
         
     def test_case_pass_3(self):
         a = -4
@@ -24,19 +26,22 @@ class Tests(unittest.TestCase):
         
         result = divide(a, b)
         self.assertEqual(result, -2.0)
+        print("test_case_pass_3 passed")
         
-    #def test_case_fail_1(self):
-    #    a = 10
-    #    b = 0
-    #    
-    #    result = divide(a, b)
-    #    
-    #def test_case_fail_2(self):
-    #    a = 100
-    #    b = 2
-    #    
-    #    result = divide(a, b)
-    #    self.assertEqual(result, 500.0)
+    def test_case_fail_1(self):
+        a = 10
+        b = 0
+        
+        result = divide(a, b)
+        print("test_case_fail_1 passed!")
+        
+    def test_case_fail_2(self):
+        a = 100
+        b = 2
+        
+        result = divide(a, b)
+        self.assertEqual(result, 500.0)
+        print("test_case_fail_2 passed!")
 
 if __name__ == '__main__':
     unittest.main()
